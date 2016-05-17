021f8a80148440f5303be0ed67bfc4c266f39196
Charmyne Mamador <charmynemamador@gmail.com>
2016-05-17 19:10:15 +0800

    build 3

---
+++ dist/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name venuesindilimanApp
+ * @description
+ * # venuesindilimanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('venuesindilimanApp', [
+    //leaflet modules
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'angular.filter',
+    'ui-leaflet',
+    // 'leaflet-directive',
+	//initial modules
+    'ngAnimate',
+    'ngAria',
+    'ngMessages',
+    'ngTouch'
+  ])
+  .config(function($routeProvider){
+  	$routeProvider
+  		.when('/',{
+  			templateUrl:'views/main.html',
+  			controller: 'MainCtrl'
+  		})
+  		.otherwise({
+  			redirectTo:'/'
+  		});
+
+  });
